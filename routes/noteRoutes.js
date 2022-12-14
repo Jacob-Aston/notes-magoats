2633c0ea0218cd599103ebdd9e2b62f9002da387
JAston <jacob.aston1@gmail.com>
2022-12-13 22:27:41 -0700

    added noteRoutes.js

---
+++ routes/noteRoutes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const db = require("../db/db.json");
+console.log(db);
+
+//post: api/notes
+
+//delete: api/notes/id
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const path = require('path');
+const noteRoutes = require("./routes/noteRoutes.js");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+app.use("/api", noteRoutes);
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
-// app.get('/paths', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/paths.html'))
-// );
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
