4f1834fd75ac86926063177707176c9922599ea0
JAston <jacob.aston1@gmail.com>
2022-12-14 17:37:07 -0700

    Added nodemon and got existing notes to appear in list

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 const db = require("../db/db.json");
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils")
 console.log(db);
 
 //post: api/notes
+router.get('/', (req, res) => {
+    console.info(`${req.method} request received for tips`);
+    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+router.get("/notes", (req, res) => res.json(db));
 
 //delete: api/notes/id
 
